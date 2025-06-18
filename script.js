d2936192b32ec40f7e4fad2c2d6f2e5685551b64
t4nel <michelkorfmacher@yahoo.de>
2025-06-18 13:18:21 +0200

    Update question display and navigation functionality in quiz

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 let questions = [
     {
-        "question": "Frage: Wie viele monster gibt es zum release von Monster Hunter Wilds?",
+        "question": "Frage: Wie viele Monster gibt es zum Release von Monster Hunter Wilds?",
         "answer_1": "24",
         "answer_2": "12",
         "answer_3": "29",
         "answer_4": "18",
         "right_answer": 3
+    },
+    {
+        "question": "Frage: Wie viele Waffentypen gibt es zum release von Monster Hunter Wilds?",
+        "answer_1": "14",
+        "answer_2": "12",
+        "answer_3": "9",
+        "answer_4": "5",
+        "right_answer": 1
     }
 ];
 
 let currentQuestion = 0;
 
 function init() {
-    document.getElementById('all_questions').innerHTML = questions.length;
     showQuestion()
 };
 
 
 function showQuestion() {
     let question = questions[currentQuestion];
+    document.getElementById('next_button').disabled = true;
+    document.getElementById('all_questions').innerHTML = questions.length;
+    document.getElementById('question_number').innerHTML = currentQuestion +1;
     document.getElementById('question_text').innerHTML = question['question']
     document.getElementById('answer_1').innerHTML = question['answer_1']
     document.getElementById('answer_2').innerHTML = question['answer_2']
@@ -39,3 +49,21 @@ function answer(selection) {
     };
     document.getElementById('next_button').disabled = false;
 };
+
+
+function nextQuestion() {
+    currentQuestion++;
+    removeAnswerHighlights();
+    showQuestion();
+};
+
+function removeAnswerHighlights() {
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
