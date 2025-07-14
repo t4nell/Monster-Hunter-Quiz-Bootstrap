76fddd638c61ed6a08da0c22ca8e27579b024488
t4nel <michelkorfmacher@yahoo.de>
2025-07-14 13:11:07 +0200

    Add audio feedback and progress tracking to quiz functionality

--- script.js
+++ script.js
@@ -83,6 +83,9 @@ let questions = [
 
 let currentQuestion = 0;
 let rightQuestions = 0;
+let AUDIO_SUCCESS = new Audio('./assets/audio/good-boy-male-voice-praise-352699.mp3');
+let AUDIO_fail = new Audio('./assets/audio/aww-cute-reaction-6208.mp3');
+
 
 function init() {
     showQuestion()
@@ -95,10 +98,18 @@ function showQuestion() {
         document.getElementById('end_screen').style = '';
         document.getElementById('question_body').style = 'display: none';
         document.getElementById('question_img').style = 'display: none';
+        document.getElementById('progress-number-container').style = `display: none`;
+        document.getElementById('progress-bar-container').style = `display: none`;
         document.getElementById('amount_of_questions').innerHTML = questions.length;
         document.getElementById('amount_of_right_questions').innerHTML = rightQuestions;
     }else {
+        let percent = (currentQuestion + 1) / questions.length;
+        percent = Math.round(percent * 100);
+        document.getElementById('progress-number').innerHTML = `${percent}%`;
+        document.getElementById('progress-bar').style.width = `${percent}%`;
+
         let question = questions[currentQuestion];
+
         document.getElementById('next_button').disabled = true;
         document.getElementById('all_questions').innerHTML = questions.length;
         document.getElementById('question_number').innerHTML = currentQuestion +1;
@@ -117,10 +128,12 @@ function answer(selection) {
     let idOfRightAnswer = `answer_${question['right_answer']}`;
     if (selectedQuestionNumber == question['right_answer']) {
         document.getElementById(selection).parentNode.classList.add('bg-success')
+        AUDIO_SUCCESS.play();
         rightQuestions++;
     }else {
         document.getElementById(selection).parentNode.classList.add('bg-danger')
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success')
+        AUDIO_fail.play();
     };
     document.getElementById('next_button').disabled = false;
 };
@@ -132,6 +145,7 @@ function nextQuestion() {
     showQuestion();
 };
 
+
 function removeAnswerHighlights() {
     document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_1').parentNode.classList.remove('bg-success');
@@ -141,4 +155,17 @@ function removeAnswerHighlights() {
     document.getElementById('answer_3').parentNode.classList.remove('bg-success');
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+};
+
+
+function restartGame() {
+    document.getElementById('end_screen').style = 'display: none';
+    document.getElementById('question_body').style = 'display: block';
+    document.getElementById('question_img').style = 'display: block';
+    document.getElementById('progress-number-container').style = 'display: block; width: 100%; height: 20px; text-align: center';
+    document.getElementById('progress-bar-container').style = 'display: block; margin-top: 4px;';
+    document.getElementById('progress-bar').style = 'display: block; height: 100%';
+    rightQuestions = 0;
+    currentQuestion = 0;
+    init();
 }
